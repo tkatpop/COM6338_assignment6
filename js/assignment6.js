0b0dbae5c07995e4e05d2ca0f01d13e530f20889
Tabitha Poppenwimer <poppenwimer.tkat@ufl.edu>
2022-04-15 17:38:35 +0300

    javascript for cheese page, replaced logo

--- js/assignment6.js
+++ js/assignment6.js
@@ -1,6 +1,20 @@
- (function () {
-    let stay = confirm("You are leaving this Web Page. Do you want to continue to leave this Web site?");
-    if (stay === false) {
-        window.location = "";
+let f_link = document.getElementById("recipes");
+f_link.addEventListener("click", function (event){
+    let is_sure = confirm("You are leaving this webpage. Are you sure you want to leave?");
+    if (is_sure === false){
+        event.preventDefault();
     }
+}, false);
+
+let title = document.getElementById("food");
+title.addEventListener("dblclick", function (){
+    window.scrollBy(0,200);
 });
+
+let r_image= ["../images/mac_n_cheese.jpg","../images/picture1.jpg","../images/picture2.jpg","../images/picture3.jpg","../images/picture4.jpg"],
+    im = document.getElementById("my_image"),
+    rand_int = Math.floor(Math.random() * 5);
+im.src = r_image[rand_int];
+
+
+
